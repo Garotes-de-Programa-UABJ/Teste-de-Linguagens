3ce2e824fe40c54b70663bf461b69b10ed0909d3
Emerson Batista <84859510+emersongg21@users.noreply.github.com>
2023-03-09 15:35:18 +0000

    Atualização

--- Angular/teste_sprint1/src/app/app-routing.module.ts
+++ Angular/teste_sprint1/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './paginas/login/login.component';
 import { PrincipalComponent } from './paginas/principal/principal.component';
-import { SignupComponent } from './paginas/signup/signup.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { AgendamentoComponent } from './paginas/agendamento/agendamento.component';
 
 const routes: Routes = [
 
   {path:'login', component:LoginComponent},
   {path:'signup', component:SignupComponent},
+  {path:'agendamento', component:AgendamentoComponent},
   {path:'principal', component:PrincipalComponent},
   {path:'**', redirectTo:'login'}
 
--- Angular/teste_sprint1/src/app/app.module.ts
+++ Angular/teste_sprint1/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './paginas/login/login.component';
-import { SignupComponent } from './paginas/signup/signup.component';
+import { SignupComponent } from './components/signup/signup.component';
 import { PrincipalComponent } from './paginas/principal/principal.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AgendamentoComponent } from './paginas/agendamento/agendamento.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FooterComponent } from './components/footer/footer.component';
     SignupComponent,
     PrincipalComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    AgendamentoComponent
   ],
   imports: [
     BrowserModule,
