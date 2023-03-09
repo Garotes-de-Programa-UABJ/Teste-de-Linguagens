8692110480b7e7bcb7f13ac5734aa77dc368df2e
San2855 <santiago.henrique@ufrpe.br>
2023-03-09 17:27:41 -0300

    implementacçao de DarkMode

--- Javascript/Teste/main.js
+++ Javascript/Teste/main.js
@@ -9,6 +9,7 @@ const telefone = document.getElementById('telefone');
 const data = document.getElementById('data');
 const horario = document.getElementById('horario');
 const tipo = document.getElementById('tipo');
+const sub = document.getElementById('sub');
 const observacoes = document.getElementById('observacoes');
 
 // Adiciona um evento de envio ao formulário
@@ -16,7 +17,7 @@ form.addEventListener('submit', function (event) {
   event.preventDefault(); // Evita que a página seja recarregada
 
   // Verifica se todos os campos foram preenchidos
-  if (nome.value !== '' && email.value !== '' && telefone.value !== '' && data.value !== '' && horario.value !== '' && senha.value !== '') {
+  if (nome.value !== '' && email.value !== '' && telefone.value !== '' && data.value !== '' && horario.value !== '' && senha.value !== '' && tipo.value !== 'none') {
     // Exibe mensagem de sucesso e limpa o formulário
     alert('Agendamento realizado com sucesso!');
     form.reset();
@@ -40,3 +41,66 @@ function mostrar_senha(){
   senha.type = "password";  
   }
 }
+
+var modoEscuro = false; // define o modo inicial como claro
+
+function alternarModo() {
+  modoEscuro = !modoEscuro; // alterna o estado da variável de controle
+  if (modoEscuro) {
+
+    document.body.style.backgroundColor = "#21262D";
+    document.body.style.color = "whitesmoke";
+
+    var nome = document.getElementById('nome');
+    var senha =  document.getElementById('senha')
+    var email = document.getElementById('email');
+    var telefone = document.getElementById('telefone');
+    var data = document.getElementById('data');
+    var horario = document.getElementById('horario');
+    var tipo = document.getElementById('tipo');
+    var observacoes = document.getElementById('observacoes');
+    var sub = document.getElementById('sub');
+
+    nome.style.backgroundColor = "#40414F";
+    senha.style.backgroundColor = "#40414F";
+    email.style.backgroundColor = "#40414F";
+    telefone.style.backgroundColor = "#40414F";
+    data.style.backgroundColor = "#40414F";
+    horario.style.backgroundColor = "#40414F";
+    tipo.style.backgroundColor = "#40414F";
+    observacoes.style.backgroundColor = "#40414F";
+    sub.style.backgroundColor = "#40414F";
+
+    document.getElementById("botao").innerHTML = "Light Mode";
+
+  } else {
+
+    document.body.style.backgroundColor = "";
+    document.body.style.color = "";
+
+    var nome = document.getElementById('nome');
+    var senha =  document.getElementById('senha')
+    var email = document.getElementById('email');
+    var telefone = document.getElementById('telefone');
+    var data = document.getElementById('data');
+    var horario = document.getElementById('horario');
+    var tipo = document.getElementById('tipo');
+    var observacoes = document.getElementById('observacoes');
+    var sub = document.getElementById('sub');
+
+
+    nome.style.backgroundColor = "";
+    senha.style.backgroundColor = "";
+    email.style.backgroundColor = "";
+    telefone.style.backgroundColor = "";
+    data.style.backgroundColor = "";
+    horario.style.backgroundColor = "";
+    tipo.style.backgroundColor = "";
+    observacoes.style.backgroundColor = "";
+    sub.style.backgroundColor = "";
+
+    document.getElementById("botao").innerHTML = "Dark Mode"; 
+    
+  }
+}
+
