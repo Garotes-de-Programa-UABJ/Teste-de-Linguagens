8b5b1b620fb3e5e6feafa7cb49b07614700b563c
San2855 <santiago.henrique@ufrpe.br>
2023-03-10 14:29:37 -0300

    Ajustes

--- Javascript/Sprint 1/main.js
+++ Javascript/Sprint 1/main.js
@@ -4,6 +4,7 @@ const form = document.querySelector('form');
 // Seleciona os elementos do formulário HTML
 const nome = document.getElementById('nome');
 const senha =  document.getElementById('senha')
+const cpf =  document.getElementById('cpf')
 const email = document.getElementById('email');
 const telefone = document.getElementById('telefone');
 const data = document.getElementById('data');
@@ -18,7 +19,7 @@ form.addEventListener('submit', function (event) {
   event.preventDefault(); // Evita que a página seja recarregada
 
   // Verifica se todos os campos foram preenchidos
-  if (nome.value !== '' && email.value !== '' && telefone.value !== '' && data.value !== '' && horario.value !== '' && senha.value !== '' && tipo.value !== 'none') {
+  if (nome.value !== '' && email.value !== '' && telefone.value !== '' && data.value !== '' && horario.value !== '' && senha.value !== '' && cpf.value !=='' && tipo.value !== 'none') {
     // Exibe mensagem de sucesso e limpa o formulário
     alert('Agendamento realizado com sucesso!');
     form.reset();
@@ -62,6 +63,8 @@ function alternarModo() {
     var observacoes = document.getElementById('observacoes');
     var sub = document.getElementById('sub');
     var bota = document.getElementById('bota');
+    var cpf =  document.getElementById('cpf')
+
 
     nome.style.backgroundColor = "#40414F";
     senha.style.backgroundColor = "#40414F";
@@ -75,6 +78,8 @@ function alternarModo() {
     bota.style.backgroundColor = "black";
     bota.style.color = "whitesmoke";
     bota.style.borderColor = "whitesmoke";
+    cpf.style.backgroundColor = "#40414F";
+
     
     document.getElementById("bot").innerHTML = "Light Mode";
 
@@ -93,6 +98,8 @@ function alternarModo() {
     var observacoes = document.getElementById('observacoes');
     var sub = document.getElementById('sub');
     var bota = document.getElementById('bota');
+    var cpf =  document.getElementById('cpf')
+
 
 
 
@@ -108,9 +115,45 @@ function alternarModo() {
     bota.style.backgroundColor = "";
     bota.style.color = ""
     bota.style.borderColor = "";
+    cpf.style.backgroundColor = "";
+
 
     document.getElementById("bot").innerHTML = "Dark Mode"; 
     
   }
 }
-
+$(document).ready(function(){
+  $('.data').mask('00/00/0000');
+  $('.horario').mask('00:00:00');
+  $('.date_time').mask('00/00/0000 00:00:00');
+  $('.cep').mask('00000-000');
+  $('.phone').mask('0000-0000');
+  $('.telefone').mask('(00) 00000-0000');
+  $('.phone_us').mask('(000) 000-0000');
+  $('.mixed').mask('AAA 000-S0S');
+  $('.cpf').mask('000.000.000-00', {reverse: true});
+  $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
+  $('.money').mask('000.000.000.000.000,00', {reverse: true});
+  $('.money2').mask("#.##0,00", {reverse: true});
+  $('.ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {
+    translation: {
+      'Z': {
+        pattern: /[0-9]/, optional: true
+      }
+    }
+  });
+  $('.ip_address').mask('099.099.099.099');
+  $('.percent').mask('##0,00%', {reverse: true});
+  $('.clear-if-not-match').mask("00/00/0000", {clearIfNotMatch: true});
+  $('.placeholder').mask("00/00/0000", {placeholder: "__/__/____"});
+  $('.fallback').mask("00r00r0000", {
+      translation: {
+        'r': {
+          pattern: /[\/]/,
+          fallback: '/'
+        },
+        placeholder: "__/__/____"
+      }
+    });
+  $('.selectonfocus').mask("00/00/0000", {selectOnFocus: true});
+});
