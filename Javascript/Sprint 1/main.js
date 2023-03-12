065392aad8540375e2d317c95a819cea7cee58ca
San2855 <santiago.henrique@ufrpe.br>
2023-03-12 20:28:15 -0300

    Projeto final

--- Javascript/Sprint 1/main.js
+++ Javascript/Sprint 1/main.js
@@ -3,8 +3,8 @@ const form = document.querySelector('form');
 
 // Seleciona os elementos do formulário HTML
 const nome = document.getElementById('nome');
-const senha =  document.getElementById('senha')
-const cpf =  document.getElementById('cpf')
+const senha = document.getElementById('senha')
+const cpf = document.getElementById('cpf')
 const email = document.getElementById('email');
 const telefone = document.getElementById('telefone');
 const data = document.getElementById('data');
@@ -19,7 +19,7 @@ form.addEventListener('submit', function (event) {
   event.preventDefault(); // Evita que a página seja recarregada
 
   // Verifica se todos os campos foram preenchidos
-  if (nome.value !== '' && email.value !== '' && telefone.value !== '' && data.value !== '' && horario.value !== '' && senha.value !== '' && cpf.value !=='' && tipo.value !== 'none') {
+  if (nome.value !== '' && email.value !== '' && telefone.value !== '' && data.value !== '' && horario.value !== '' && senha.value !== '' && cpf.value !== '' && tipo.value !== 'none') {
     // Exibe mensagem de sucesso e limpa o formulário
     alert('Agendamento realizado com sucesso!');
     form.reset();
@@ -29,32 +29,17 @@ form.addEventListener('submit', function (event) {
   }
 });
 
-// Redireciona o usuário para a página de registro
-function mostrarSignup() {
-  window.location.href = 'signup.html';
-}
-
-// Alterna a exibição da senha
-function mostrar_senha(){
-  var senha = document.getElementById("senha");
-  if (senha.type == "password"){
-    senha.type = "text";
-  }else{
-  senha.type = "password";  
-  }
-}
-
 var modoEscuro = false; // define o modo inicial como claro
 
 function alternarModo() {
   modoEscuro = !modoEscuro; // alterna o estado da variável de controle
   if (modoEscuro) {
-
+    // Alterna para as cores do modo escuro
     document.body.style.backgroundColor = "#21262D";
     document.body.style.color = "whitesmoke";
 
     var nome = document.getElementById('nome');
-    var senha =  document.getElementById('senha')
+    var senha = document.getElementById('senha')
     var email = document.getElementById('email');
     var telefone = document.getElementById('telefone');
     var data = document.getElementById('data');
@@ -63,8 +48,7 @@ function alternarModo() {
     var observacoes = document.getElementById('observacoes');
     var sub = document.getElementById('sub');
     var bota = document.getElementById('bota');
-    var cpf =  document.getElementById('cpf')
-
+    var cpf = document.getElementById('cpf')
 
     nome.style.backgroundColor = "#40414F";
     senha.style.backgroundColor = "#40414F";
@@ -80,16 +64,16 @@ function alternarModo() {
     bota.style.borderColor = "whitesmoke";
     cpf.style.backgroundColor = "#40414F";
 
-    
+
     document.getElementById("bot").innerHTML = "Light Mode";
 
   } else {
-
+    // Alterna para as cores do modo claro
     document.body.style.backgroundColor = "";
     document.body.style.color = "";
 
     var nome = document.getElementById('nome');
-    var senha =  document.getElementById('senha')
+    var senha = document.getElementById('senha')
     var email = document.getElementById('email');
     var telefone = document.getElementById('telefone');
     var data = document.getElementById('data');
@@ -98,10 +82,7 @@ function alternarModo() {
     var observacoes = document.getElementById('observacoes');
     var sub = document.getElementById('sub');
     var bota = document.getElementById('bota');
-    var cpf =  document.getElementById('cpf')
-
-
-
+    var cpf = document.getElementById('cpf')
 
     nome.style.backgroundColor = "";
     senha.style.backgroundColor = "";
@@ -117,9 +98,12 @@ function alternarModo() {
     bota.style.borderColor = "";
     cpf.style.backgroundColor = "";
 
+    document.getElementById("bot").innerHTML = "Dark Mode";
 
-    document.getElementById("bot").innerHTML = "Dark Mode"; 
-    
   }
 }
-
+// Adiciona uma máscara para limitar o preenchimento de alguns campos
+$('#telefone').mask('(00) 00000-0000');
+$('#data').mask('00/00/0000');
+$('#horario').mask('00:00');
+$('#cpf').mask('000.000.000-00', { reverse: true });
