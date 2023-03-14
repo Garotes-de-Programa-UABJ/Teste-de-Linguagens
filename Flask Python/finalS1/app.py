bcf212d3bb7056d3d0d420b9618e56b44314e972
Karol Araújo <78654484+kxaraujo@users.noreply.github.com>
2023-03-13 23:16:27 -0300

    adicionando comentários em cada trecho de código

--- Flask Python/finalS1/app.py
+++ Flask Python/finalS1/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request
 import sqlite3
 
+#Importa os módulos necessários para a aplicação Flask, incluindo a classe Flask, render_template para renderização de templates HTML e sqlite3 para trabalhar com um banco de dados SQLite.
+
 app = Flask(__name__)
+
+#Cria uma instância do objeto Flask para a aplicação.
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+#Define uma rota com o endereço '/' para a função home(), que retorna um template HTML chamado "home.html".
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
@@ -19,3 +25,4 @@ def add_user():
     conn.close()
     return "Usuário cadastrado com sucesso"
 
+#Define uma rota com o endereço '/add_user' e método POST para a função add_user(). Essa função abre uma conexão com um banco de dados SQLite, extrai os dados do formulário HTML usando o objeto request, insere as informações do usuário no banco de dados e retorna uma mensagem indicando que o usuário foi cadastrado com sucesso.
